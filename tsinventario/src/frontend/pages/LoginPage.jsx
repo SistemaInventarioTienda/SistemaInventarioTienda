fdb976437694bf4fe6ff568b7150ae04b51083b7
AaronMatarrita <aaron.matarrita.portuguez@est.una.ac.cr>
2024-10-12 22:31:28 -0600

    style: updated styles in LoginPage

--- tsinventario/src/frontend/pages/LoginPage.jsx
+++ tsinventario/src/frontend/pages/LoginPage.jsx
@@ -1,14 +1,15 @@
-import { useAuth } from "../context/authContext";
-import { Link, useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { useEffect } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Card } from "../components/ui";
-import { loginSchema } from "../schemas/auth";
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useAuth } from '../context/authContext';
+import { loginSchema } from '../schemas/auth';
+import { Card } from '../components/ui';
+import { ShoppingBag, User, Lock, Eye, EyeOff } from 'lucide-react'; // Ojo y ojo tachado para el icono de mostrar/ocultar
+import './css/LoginPage.css'; // Importa el archivo CSS específico para la página de login
 
 export function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false); // Estado para controlar la visibilidad de la contraseña
   const {
     register,
     handleSubmit,
@@ -22,85 +23,79 @@ export function LoginPage() {
   const onSubmit = (data) => signin(data);
 
   useEffect(() => {
-    document.title = "Inicio Sesión";
+    document.title = 'Iniciar Sesión';
     if (isAuthenticated) {
-      navigate("/");
+      navigate('/');
     }
-  }, [isAuthenticated]);
-  
-  return (
-    <div className="h-[calc(100vh-100px)] flex items-center justify-center">
-    <div className="hold-transition login-page">
-      <div className="login-box" style={ {width: "400px"}}>
-        <Card>
-          <div className="card card-outline">
-            <div className="card-header text-center bg-white">
-              <h1><b>Iniciar</b> Sesión</h1>
-              <img
-                className="img-thumbnail"
-                src="https://solucionsistemas.com/salesphp/Assets/img/logo.png"
-                width="150"
-                alt="Logo"
-              />
-            </div>
-            <div className="card-body">
-              <form id="frmLogin" onSubmit={handleSubmit(onSubmit)}>
-              {loginErrors && (
-                  <div className="mt-3">
-                    {loginErrors.map((error, i) => (
-                      <p className="text-danger" key={i}>{error}</p>
-                    ))}
-                  </div>
-                )}
-                <div className="input-group mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Usuario"
-                    name="DSC_NOMBREUSUARIO"
-                    id="usuario"
-                    {...register("DSC_NOMBREUSUARIO")}
-                  />
-                  <div className="input-group-append">
-                    <div className="input-group-text">
-                      <i className="bi bi-person-fill"></i>
-                    </div>
-                  </div>
-                  <p className="text-danger">{errors.DSC_NOMBREUSUARIO?.message}</p>
-                </div>
+  }, [isAuthenticated, navigate]);
 
-                <div className="input-group mb-3">
-                  <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Contraseña"
-                    name="DSC_CONTRASENIA"
-                    id="clave"
-                    {...register("DSC_CONTRASENIA")}
-                  />
-                  <div className="input-group-append">
-                    <div className="input-group-text">
-                      <i className="bi bi-lock-fill"></i>
-                    </div>
-                  </div>
-                  <p className="text-danger">{errors.DSC_CONTRASENIA?.message}</p>
-                </div>
-                <button
-                  type="submit"
-                  className="btn btn-primary btn-block"
-                  id="btnAccion"
-                >
-                  Iniciar Sesión
-                </button>
-              </form>
-            </div>
+  return (
+    <div className="h-[calc(100vh-100px)] flex items-center justify-center bg-secondary-custom border-custom">
+      <Card className="login-card shadow-lg">
+        <div className="card-body p-5">
+          <div className="text-center mb-4">
+            <ShoppingBag size={48} className="mb-2 text-primary-custom" />
+            <h2 className="fw-bold text-primary-custom">Inicio de Sesión</h2>
           </div>
-        </Card>
-      </div>
-    </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+
+            <div className="mb-3">
+              <div className="input-group">
+                <span className="input-group-text bg-primary-custom text-secondary-custom">
+                  <User size={18} />
+                </span>
+                <input
+                  type="text"
+                  className={`form-control border-custom ${errors.DSC_NOMBREUSUARIO ? 'is-invalid' : ''}`}
+                  placeholder="Usuario"
+                  {...register('DSC_NOMBREUSUARIO')}
+                />
+              </div>
+              {errors.DSC_NOMBREUSUARIO && (
+                <div className="invalid-feedback d-block">{errors.DSC_NOMBREUSUARIO.message}</div>
+              )}
+            </div>
+
+            <div className="mb-4">
+              <div className="input-group">
+                <span className="input-group-text bg-primary-custom text-secondary-custom">
+                  <Lock size={18} />
+                </span>
+                <input
+                  type={showPassword ? 'text' : 'password'} // Cambia el tipo de input entre password y text
+                  className={`form-control border-custom ${errors.DSC_CONTRASENIA ? 'is-invalid' : ''}`}
+                  placeholder="Contraseña"
+                  {...register('DSC_CONTRASENIA')}
+                />
+                <button id="paswordField"
+                  type="button"
+                  className="btn"
+                  onClick={() => setShowPassword(!showPassword)} // Alterna entre mostrar y ocultar
+                >
+                  {showPassword ? <EyeOff size={18} /> : <Eye size={18} />} {}
+                </button>
+              </div>
+              {errors.DSC_CONTRASENIA && (
+                <div className="invalid-feedback d-block">{errors.DSC_CONTRASENIA.message}</div>
+              )}
+            </div>
+
+            {loginErrors && loginErrors.map((error, i) => (
+              <div key={i} className="alert alert-custom">{error}</div>
+            ))}
+
+            <button
+              type="submit"
+              className="btn btn-primary w-100 border-custom"
+            >
+              Iniciar Sesión
+            </button>
+
+          </form>
+        </div>
+      </Card>
     </div>
   );
 }
 
 export default LoginPage;
-
