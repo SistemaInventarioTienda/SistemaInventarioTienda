8bcec5e09d327698f3789456a686b4054444a8b0
AaronMatarrita <aaron.matarrita.portuguez@est.una.ac.cr>
2024-10-21 18:56:00 -0600

    fix: resolve horizontal and vertical scroll issue on main layout

--- tsinventario/src/frontend/components/Navbar.jsx
+++ tsinventario/src/frontend/components/Navbar.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Bell, User } from 'lucide-react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Bell, UserRoundCogIcon } from 'lucide-react';
 import { Button } from './ui';
 import { useAuth } from "../context/authContext";
 import UserMenu from './ui/UserMenu';
@@ -7,14 +7,33 @@ import UserMenu from './ui/UserMenu';
 const Navbar = () => {
   const { isAuthenticated, user } = useAuth();
   const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
-
-  if (!isAuthenticated) {
-    return null;
-  }
+  const userMenuRef = useRef(null);
 
   const toggleUserMenu = () => {
     setIsUserMenuOpen(prev => !prev);
   };
+
+  const handleClickOutside = (event) => {
+    if (userMenuRef.current && !userMenuRef.current.contains(event.target)) {
+      setIsUserMenuOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isUserMenuOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isUserMenuOpen]);
+
+  if (!isAuthenticated) {
+    return null;
+  }
 
   return (
     <nav className="navbar bg-card-custom" style={{ height: '60px', padding: '0 30px' }}>
@@ -51,13 +70,16 @@ const Navbar = () => {
             alignItems: 'center',
             justifyContent: 'center'
           }}>
-          <User size={20} color="#FFFFFF" />
+          <UserRoundCogIcon size={20} color="#FFFFFF" />
         </Button>
 
-        <UserMenu isOpen={isUserMenuOpen} />
+        {/* El menú de usuario */}
+        <div ref={userMenuRef}>
+          <UserMenu isOpen={isUserMenuOpen} />
+        </div>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
