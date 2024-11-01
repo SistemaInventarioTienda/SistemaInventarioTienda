a5fe11df5c53d1af3e0738921df02b41d3991cf8
Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2024-11-01 16:16:03 -0600

    Added functionality to get all clients

--- tsinventario/src/backend/controllers/client.controller.js
+++ tsinventario/src/backend/controllers/client.controller.js
@@ -16,7 +16,7 @@ export const registerClient = async (req, res) => {
       DSC_APELLIDODOS: DSC_APELLIDODOS,
       FEC_CREADOEN: creadoEn,
       ESTADO: ESTADO,
-      URL_FOTO: FOTO ? `public/Assets/image/clientes/${DSC_CEDULA}.png` : `public/Assets/image/clientes/default_client.jpeg`,
+      URL_FOTO: `public/Assets/image/clientes/${DSC_CEDULA}.png`,
       DSC_DIRECCION: DSC_DIRECCION
     });
 
@@ -52,5 +52,48 @@ export const registerClient = async (req, res) => {
     } else {
       res.status(500).json({ errorMessage: error });
     }
+  }
+};
+
+export const getAllClients = async (req, res) => {
+  try {
+      // Obtén los parámetros de paginación de la solicitud (página y cantidad por página)
+      const { page = 1, pageSize = 5, orderByField = 'DSC_CEDULA', order = 'asc' } = req.query;
+      const limit = parseInt(pageSize);
+      const offset = (parseInt(page) - 1) * limit;
+
+      const field = (
+          orderByField === 'DSC_NOMBRE' || orderByField === 'ESTADO' || orderByField === 'DSC_CEDULA' || orderByField === 'TELEFONO' || 
+          orderByField === 'DSC_APELLIDOUNO' || orderByField === 'DSC_APELLIDODOS'
+          ) ? orderByField : 'DSC_CEDULA';
+
+      const sortOrder = order.toLowerCase() === 'asc' || order.toLowerCase() === 'desc' ? order : 'asc';
+      const { count, rows } = await Client.findAndCountAll({
+          attributes: {
+              exclude: ['FEC_MODIFICADOEN', 'ID_CLIENTE', 'FEC_CREADOEN']
+          },
+          limit,
+          offset,
+          order: [
+              [field, sortOrder],
+          ]
+      });
+      
+
+      if (rows.length === 0) {
+          return res.status(204).json({
+              message: "No se encontraron clientes.",
+          });
+      }
+
+      res.json({
+          total: count,
+          totalPages: Math.ceil(count / limit),
+          currentPage: parseInt(page),
+          pageSize: limit,
+          clients: rows
+      });
+  } catch (error) {
+      return res.status(500).json({ message: error.message });
   }
 };
--- tsinventario/src/backend/routes/client.routes.js
+++ tsinventario/src/backend/routes/client.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerClient } from "../controllers/client.controller.js";
+import { registerClient, getAllClients } from "../controllers/client.controller.js";
 import { auth } from "../middlewares/auth.middleware.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { registerSchema } from "../schemas/client.schema.js";
@@ -7,5 +7,6 @@ import { registerSchema } from "../schemas/client.schema.js";
 const router = Router();
 // Parte de administración de clientes
 router.post("/register", auth, validateSchema(registerSchema), registerClient);
+router.get("/all_clients", auth, getAllClients);
 
 export default router;
