7570fb9a893fa955096e57c4b5652cec913cca45
Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2024-12-30 21:51:22 -0600

    Update user.controller.js
    Se agrega la verificación al eliminar. No se puede eliminar el usuario administrador ni puede eliminar su propia cuenta.

--- tsinventario/src/backend/controllers/user.controller.js
+++ tsinventario/src/backend/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { encryptData } from "../libs/encryptData.js";
 import { validateUpdate } from "../logic/user/user.logic.js";
 import { validateUpdateUser } from "../logic/validateFields.logic.js";
 import { Op } from 'sequelize';
+import { decodedToken } from "../libs/jwt.js";
 
 export const updateUser = async (req, res) => {
     try {
@@ -12,7 +13,7 @@ export const updateUser = async (req, res) => {
         } = req.body;
 
         const validateFields = validateUpdateUser(req);
-        if(validateFields !== true) {
+        if (validateFields !== true) {
             return res.status(400).json({
                 message: validateFields,
             })
@@ -32,12 +33,12 @@ export const updateUser = async (req, res) => {
 
         // hashing the password
         var passwordHash;
-        if(DSC_CONTRASENIA){
+        if (DSC_CONTRASENIA) {
             passwordHash = await encryptData(DSC_CONTRASENIA, 10);
         } else {
             passwordHash = user.DSC_CONTRASENIA;
         }
-        
+
 
         await user.update({
             DSC_NOMBREUSUARIO, DSC_CORREO: DSC_CORREO.toLowerCase(), DSC_CONTRASENIA: passwordHash, DSC_TELEFONO, ID_ROL, DSC_CEDULA,
@@ -54,12 +55,28 @@ export const deleteUser = async (req, res) => {
     try {
         const user = await User.findOne({
             attributes: {
-              exclude: ['DSC_CONTRASENIA', 'FEC_CREADOEN']
+                exclude: ['DSC_CONTRASENIA', 'FEC_CREADOEN']
             },
             where: { DSC_CEDULA: req.params.id }
-          });
+        });
         if (!user) {
             return res.status(404).json({ message: "Usuario no encontrado." });
+        }
+
+        const { token } = req.cookies;
+        const decoded = await decodedToken(token);
+        if (user.DSC_CEDULA === decoded.id) {
+            /*
+             * Authenticated user cannot delete his own account
+             * 
+            */
+            return res.status(403).json({ message: "Tu cuenta no puede ser eliminada. Contacta con soporte para más información." })
+        } else if (user.ID_USUARIO === 10) { //Cambiar por el id del administrador debe ser 1
+            /*
+             * Cannot delete the administrator user
+             * 
+            */
+            return res.status(403).json({ message: "No tienes permiso para realizar esta acción." })
         }
 
         await user.update(
@@ -85,9 +102,9 @@ export const getAllUsers = async (req, res) => {
         const offset = (parseInt(page) - 1) * limit;
 
         const field = (
-            orderByField === 'DSC_NOMBRE' || orderByField === 'ESTADO' || orderByField === 'DSC_CEDULA' || orderByField === 'DSC_NOMBREUSUARIO' || 
+            orderByField === 'DSC_NOMBRE' || orderByField === 'ESTADO' || orderByField === 'DSC_CEDULA' || orderByField === 'DSC_NOMBREUSUARIO' ||
             orderByField === 'DSC_APELLIDOUNO' || orderByField === 'DSC_APELLIDODOS'
-            ) ? orderByField : 'DSC_CEDULA';
+        ) ? orderByField : 'DSC_CEDULA';
 
         const sortOrder = order.toLowerCase() === 'asc' || order.toLowerCase() === 'desc' ? order : 'asc';
         const { count, rows } = await User.findAndCountAll({
@@ -100,7 +117,7 @@ export const getAllUsers = async (req, res) => {
                 [field, sortOrder],
             ]
         });
-        
+
 
         if (rows.length === 0) {
             return res.status(204).json({
@@ -128,12 +145,12 @@ export const searchUser = async (req, res) => {
         const offset = (parseInt(page) - 1) * limit;
 
         const field = (
-            orderByField === 'DSC_NOMBRE' || orderByField === 'ESTADO' || orderByField === 'DSC_CEDULA' || orderByField === 'DSC_NOMBREUSUARIO' || 
+            orderByField === 'DSC_NOMBRE' || orderByField === 'ESTADO' || orderByField === 'DSC_CEDULA' || orderByField === 'DSC_NOMBREUSUARIO' ||
             orderByField === 'DSC_APELLIDOUNO' || orderByField === 'DSC_APELLIDODOS'
-            ) ? orderByField : 'DSC_CEDULA';
+        ) ? orderByField : 'DSC_CEDULA';
 
         const sortOrder = order.toLowerCase() === 'asc' || order.toLowerCase() === 'desc' ? order : 'asc';
-        const expectedMatch = { [Op.like]: `%${termSearch}%` }; 
+        const expectedMatch = { [Op.like]: `%${termSearch}%` };
         const { count, rows } = await User.findAndCountAll({
             attributes: {
                 exclude: ['DSC_CONTRASENIA', 'ID_USUARIO', 'FEC_CREADOEN']
