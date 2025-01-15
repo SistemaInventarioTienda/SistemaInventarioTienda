50994a8b740cb944a26a6d58d0dfc362a3e1575c
AaronMatarrita <aaron.matarrita.portuguez@est.una.ac.cr>
2025-01-14 18:32:31 -0600

    Update useSorting.js

--- ProyectoIngenieria/frontend/src/hooks/useSorting.js
+++ ProyectoIngenieria/frontend/src/hooks/useSorting.js
@@ -5,9 +5,12 @@ export const useSorting = (defaultField = null, defaultOrder = 'asc') => {
     const [sortOrder, setSortOrder] = useState(defaultOrder);
 
     const toggleSortOrder = (field) => {
-        const newOrder = field === sortField && sortOrder === 'asc' ? 'desc' : 'asc';
-        setSortField(field);
-        setSortOrder(newOrder);
+        if (field === sortField) {
+            setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+        } else {
+            setSortField(field);
+            setSortOrder('desc');
+        }
     };
 
     return {
