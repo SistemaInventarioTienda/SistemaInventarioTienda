8b8e58ae1069ee20088f306cbfbda5d6787eb5ba
Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2025-02-20 22:18:11 -0600

    Update config.js
    Added API_URL_RESOURCES

--- ProyectoIngenieria/frontend/src/config.js
+++ ProyectoIngenieria/frontend/src/config.js
@@ -1,1 +1,4 @@
 export const API_URL = "http://localhost:4000/api";
+export const API_URL_RESOURCES = "http://localhost:4000/uploads";
+// Se debe usar de la siguiente forma:
+// `${API_URL_RESOURCES}/images/products/${product.URL_IMAGEN}`
