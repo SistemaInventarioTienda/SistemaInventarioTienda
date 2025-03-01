b9ae04b77fadf377e9cea3be5dab4e95a09b2ed5
Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2025-02-28 22:17:50 -0600

    Update app.js

--- ProyectoIngenieria/backend/app.js
+++ ProyectoIngenieria/backend/app.js
@@ -9,6 +9,7 @@ import categoryRoutes from "./routes/category.routes.js";
 import supplierRoutes from "./routes/supplier.routes.js";
 import clientRoutes from "./routes/client.routes.js";
 import productRoutes from "./routes/product.routes.js";
+import shoppingRoutes from "./routes/shopping.routes.js";
 
 import { FRONTEND_URL } from "./config.js";
 
@@ -31,6 +32,7 @@ app.use("/api/category", categoryRoutes);
 app.use("/api/supplier", supplierRoutes);
 app.use("/api/client", clientRoutes);
 app.use("/api/product", productRoutes);
+app.use("/api/shopping", shoppingRoutes);
 app.use('/uploads', express.static('uploads'));
 
 
