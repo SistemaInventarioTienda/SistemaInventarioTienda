dc3a108b42a1b81ea820434e66040e2f1339f59d
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-03-20 23:25:18 -0600

    20-03-25 HU: SHI-28 - Task: SDI-63 done, add getAllSale function

--- ProyectoIngenieria/backend/routes/sale.routes.js
+++ ProyectoIngenieria/backend/routes/sale.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {createSale} from "../controllers/sale.controller.js";
+import {createSale,getAllSales,getSaleDetails} from "../controllers/sale.controller.js";
 import { auth } from "../middlewares/auth.middleware.js";
 import { validateSchema } from "../middlewares/validator.middleware.js";
 import { saleSchema,detailSchema } from "../schemas/sale.schema.js";
@@ -7,6 +7,8 @@ import { saleSchema,detailSchema } from "../schemas/sale.schema.js";
 const router = Router();
 
 router.post("/addSale",validateSchema(saleSchema),createSale);
+router.get("/getSales",getAllSales);
+router.post("/getOnlySale",getSaleDetails)
 
 
 export default router;
