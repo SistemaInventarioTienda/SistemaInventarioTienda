2d70cab60cc01d9e77f5bbb2e1392bb8ebfe055a
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-03-31 10:42:43 -0600

    31-03-25  SDI-75 - Task: SDI-81 Add modify payment with time

--- ProyectoIngenieria/backend/controllers/credit.controller.js
+++ ProyectoIngenieria/backend/controllers/credit.controller.js
@@ -1,4 +1,4 @@
-import {credit, payment } from "../models/sale.model.js";
+import { credit, payment } from "../models/sale.model.js";
 import { getDateCR } from "../libs/date.js";
 
 
@@ -6,16 +6,16 @@ import { getDateCR } from "../libs/date.js";
 
 export const addPayment = async (req, res) => {
     try {
-        const {MON_ABONADO} = req.body;
-        const date=await getDateCR();
+        const { MON_ABONADO } = req.body;
+        const date = await getDateCR();
         const creditId = await credit.findOne({
-            where: { 
+            where: {
                 ID_CREDITO: req.params.id,
                 ESTADO_CREDITO: 1
             }
         });
-        
-        if(!creditId){
+
+        if (!creditId) {
             return res.status(404).json({
                 message: "Credito no disponible para abonar."
             })
@@ -29,25 +29,25 @@ export const addPayment = async (req, res) => {
             return res.status(400).json({ message: 'Monto no permitido' });
         }
 
-        const payRegist= new payment({
+        const payRegist = new payment({
             ID_CREDITO: creditId.ID_CREDITO,
             FEC_ABONO: date,
             MON_ABONADO
         }
         )
 
-        const addPay= payRegist.save();
+        const addPay = payRegist.save();
 
         if (addPay) {
             creditId.MON_PENDIENTE -= MON_ABONADO;
-            if( creditId.MON_PENDIENTE ===0){
+            if (creditId.MON_PENDIENTE === 0) {
                 creditId.ESTADO_CREDITO = 0;
             }
-            creditId.FEC_ULTIMOPAGO=date;
+            creditId.FEC_ULTIMOPAGO = date;
             await creditId.save();
-            res.status(201).json({ message: 'Abono registrado Correctamente' });   
+            res.status(201).json({ message: 'Abono registrado Correctamente' });
         }
- 
+
     } catch (error) {
         res.status(500).json({ message: 'Error al realizar el abono del credito', error });
     }
@@ -58,45 +58,48 @@ export const addPayment = async (req, res) => {
 
 export const modifyPayment = async (req, res) => {
     try {
-        const {MON_ABONADO} = req.body;
-        const date=await getDateCR();
+        const { MON_ABONADO } = req.body;
+        const date = await getDateCR();
         const paymentObj = await payment.findOne({
-            where: { 
+            where: {
                 ID_ABONO: req.params.id
             }
         });
-        if(!paymentObj){
+        if (!paymentObj) {
             return res.status(404).json({
                 message: "El Abono seleccionado no se encuentra en el sistema."
             })
         }
-/*
-        const paymentDate = new Date(paymentObj.FEC_ABONO);
-        const now = new Date(date);
-        console.log(now)
-        const differenceInMinutes = (now - paymentDate) / (1000 * 60);
-        console.log(differenceInMinutes)
-//1440
-        if (differenceInMinutes > 30) {
+
+        
+
+        const fechaAbono = new Date(paymentObj.FEC_ABONO);
+        const fechaActual = new Date(await getDateCR());
+
+        const diferenciaMs = Math.abs(fechaActual - fechaAbono);
+        const diferenciaMinutos = diferenciaMs / (1000 * 60);
+
+
+        if (diferenciaMinutos > 30) {
             return res.status(400).json({
-                message: "No se puede procesar el abono, han pasado más de 30 minutos desde el abono, ."
+                message: "No se puede procesar el abono, tiempo limite excedido."
             });
         }
-*/
+
         const creditId = await credit.findOne({
-            where: { 
+            where: {
                 ID_CREDITO: paymentObj.ID_CREDITO,
                 ESTADO_CREDITO: 1
             }
         });
-        if(!creditId){
+        if (!creditId) {
             return res.status(404).json({
                 message: "Credito no disponible para abonar."
             })
         }
-        
-        creditId.MON_PENDIENTE+=paymentObj.MON_ABONADO;
-       
+
+        creditId.MON_PENDIENTE += paymentObj.MON_ABONADO;
+
 
         if (MON_ABONADO > creditId.MON_PENDIENTE) {
             return res.status(400).json({ message: 'El monto a rebajar es mayor que el saldo pendiente' });
@@ -106,23 +109,23 @@ export const modifyPayment = async (req, res) => {
             return res.status(400).json({ message: 'Monto no permitido' });
         }
 
-       
-        paymentObj.FEC_ABONO=date;
-        paymentObj.MON_ABONADO=MON_ABONADO;
+
+        paymentObj.FEC_ABONO = date;
+        paymentObj.MON_ABONADO = MON_ABONADO;
 
 
-        const addPay= paymentObj.save();
+        const addPay = paymentObj.save();
 
         if (addPay) {
             creditId.MON_PENDIENTE -= MON_ABONADO;
-            if( creditId.MON_PENDIENTE ===0){
+            if (creditId.MON_PENDIENTE === 0) {
                 creditId.ESTADO_CREDITO = 0;
             }
-            creditId.FEC_ULTIMOPAGO=date;
+            creditId.FEC_ULTIMOPAGO = date;
             await creditId.save();
-            res.status(201).json({ message: 'Abono actualizado correctamente' });   
+            res.status(201).json({ message: 'Abono actualizado correctamente' });
         }
- 
+
     } catch (error) {
         res.status(500).json({ message: 'Error al realizar el abono del credito', error });
     }
