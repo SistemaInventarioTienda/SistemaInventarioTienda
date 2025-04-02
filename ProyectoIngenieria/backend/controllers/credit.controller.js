20d294b9377fe1cc62b1bf46c86e2dd00128d0dd
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-04-01 19:11:26 -0600

    01-04-25  HU: SDI-75 - Task: SDI-84

--- ProyectoIngenieria/backend/controllers/credit.controller.js
+++ ProyectoIngenieria/backend/controllers/credit.controller.js
@@ -1,4 +1,5 @@
-import { credit, payment } from "../models/sale.model.js";
+import { credit, payment,sale } from "../models/sale.model.js";
+import Client from "../models/client.model.js";
 import { getDateCR } from "../libs/date.js";
 
 
@@ -128,5 +129,61 @@ export const modifyPayment = async (req, res) => {
 
     } catch (error) {
         res.status(500).json({ message: 'Error al realizar el abono del credito', error });
+    }
+};
+
+
+
+
+
+export const getAllPaymentByCredit = async (req, res) => {
+    try {
+        const { page = 1, pageSize = 5, orderByField = 'FEC_VENCIMIENTO', order = 'desc' } = req.query;
+        const limit = parseInt(pageSize);
+        const offset = (parseInt(page) - 1) * limit;
+
+        const field = ['FEC_VENCIMIENTO', 'ESTADO_CREDITO', 'MON_PENDIENTE','FEC_ULTIMOPAGO'].includes(orderByField) ? orderByField : 'FEC_VENTA';
+        const sortOrder = order.toLowerCase() === 'asc' || order.toLowerCase() === 'desc' ? order : 'asc';
+
+        const { count, rows } = await credit.findAndCountAll({
+            attributes: { exclude: [] },
+            limit,
+            offset,
+            order: [[field, sortOrder]],
+            include: [
+                {
+                    model: sale, 
+                    attributes: ['ID_VENTA'],
+                    include: [
+                        {
+                            model: Client,
+                            attributes: ['DSC_NOMBRE']
+                        }
+                    ]
+                },
+                {
+                    model: payment,
+                    attributes: ['ID_ABONO','FEC_ABONO', 'MON_ABONADO'],
+                }
+            ],
+            distinct: true
+        });
+
+        if (rows.length === 0) {
+            return res.status(204).json({
+                message: "No se encontraron abonos realizados.",
+            });
+        }
+
+       
+        res.json({
+            total: count,
+            totalPages: Math.ceil(count / limit),
+            currentPage: parseInt(page),
+            pageSize: limit,
+            sales: rows,
+        });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
     }
 };
