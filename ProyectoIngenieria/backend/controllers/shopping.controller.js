291067e5c8fcd021b00ddc5127879ce9a2b32fa4
AaronMatarrita <aaron.matarrita.portuguez@est.una.ac.cr>
2025-04-08 12:56:20 -0600

    Update shopping.controller.js

--- ProyectoIngenieria/backend/controllers/shopping.controller.js
+++ ProyectoIngenieria/backend/controllers/shopping.controller.js
@@ -230,7 +230,7 @@ export const deleteShopping = async (req, res) => {
         }
 
         const currentDate = await getDateCR();
-        if (EightDaysHavePassed(shopping.FEC_COMPRA)) {
+        if (!EightDaysHavePassed(shopping.FEC_COMPRA)) {
             return res.status(404).json({ message: "El tiempo para anular la venta ha expirado." });
         }
         await shopping.update(
