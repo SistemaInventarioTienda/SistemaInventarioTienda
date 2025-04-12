ed23db42b25ed52f1c2d7550a04e01357725c9ea
Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2025-04-11 23:58:13 -0600

    11/04/2025 SDI:104
    Added function to filter transactions

--- ProyectoIngenieria/backend/routes/transaction.routes.js
+++ ProyectoIngenieria/backend/routes/transaction.routes.js
@@ -8,5 +8,6 @@ const router = Router();
 // Parte de administración de usuarios
 router.post("/create_transaction", auth, validateSchema(registerSchema), createTransaction);
 router.get("/get_all_transaction", auth, getAllTransactions);
+router.get("/search_transaction", searchTransaction);
 
 export default router;
