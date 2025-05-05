7cfce5e4dda033425332ed95219485e9e93736b5
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-05-04 22:52:10 -0600

    update info in config

--- ProyectoIngenieria/backend/config.js
+++ ProyectoIngenieria/backend/config.js
@@ -10,10 +10,10 @@ export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 // DATABASE
 const user = process.env.USER_DB || 'root';
-const password = process.env.PASSWORD_DB || '';
-const host = process.env.HOST_DB || 'localhost';
-const port = process.env.PORT_DB || 3306;
-const database = process.env.DATABASE || 'dbtiendasistemainventario';
+const password = process.env.PASSWORD_DB || 'gRDAvfgNczUQkIUeRGYKSTYJlRRmKrcZ';
+const host = process.env.HOST_DB || 'hopper.proxy.rlwy.net';
+const port = process.env.PORT_DB || 43249;
+const database = process.env.DATABASE || 'railway';
 export const MYSQL_URI = `mysql://${user}:${password}@${host}:${port}/${database}`;
 
 // EMAIL
