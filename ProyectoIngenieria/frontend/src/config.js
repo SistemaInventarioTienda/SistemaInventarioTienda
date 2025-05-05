d699dd83cf0378d4a9d61c33f9ad75ed02e133d5
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-05-04 23:01:34 -0600

    add vercel-access

--- ProyectoIngenieria/frontend/src/config.js
+++ ProyectoIngenieria/frontend/src/config.js
@@ -1,4 +1,4 @@
-export const API_URL = "http://localhost:4000/api";
-export const API_URL_RESOURCES = "http://localhost:4000/uploads";
+export const API_URL = "https://inventariobackend-nu.vercel.app/api";
+export const API_URL_RESOURCES = "https://inventariobackend-nu.vercel.app/uploads";
 // Se debe usar de la siguiente forma:
 // `${API_URL_RESOURCES}/images/products/${product.URL_IMAGEN}`
