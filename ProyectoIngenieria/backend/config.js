1fc4d20ddcbaf9757949496e38c48888ff003043
Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2025-05-06 13:41:21 -0600

    Update config.js

--- ProyectoIngenieria/backend/config.js
+++ ProyectoIngenieria/backend/config.js
@@ -10,10 +10,10 @@ export const FRONTEND_URL = process.env.FRONTEND_URL || "https://sistema-inventa
 
 // DATABASE
 const user = process.env.USER_DB || 'root';
-const password = process.env.PASSWORD_DB || 'gRDAvfgNczUQkIUeRGYKSTYJlRRmKrcZ';
-const host = process.env.HOST_DB || 'hopper.proxy.rlwy.net';
-const port = process.env.PORT_DB || 43249;
-const database = process.env.DATABASE || 'railway';
+const password = process.env.PASSWORD_DB || '';
+const host = process.env.HOST_DB || 'localhost';
+const port = process.env.PORT_DB || 3306;
+const database = process.env.DATABASE || 'dbtiendasistemainventario';
 export const MYSQL_URI = `mysql://${user}:${password}@${host}:${port}/${database}`;
 
 // EMAIL
