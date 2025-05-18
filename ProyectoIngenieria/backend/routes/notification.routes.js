10ef24ee3cebea4d1c52ea62ec03563ad05da4d2
Josue_Porras <131841402+ItsJossscr@users.noreply.github.com>
2025-05-18 17:59:21 -0600

    add getNotifications

--- ProyectoIngenieria/backend/routes/notification.routes.js
+++ ProyectoIngenieria/backend/routes/notification.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.middleware.js";
 
-import { updateNotification } from "../controllers/notification.controller.js";
+import { updateNotification,getAllNotifications } from "../controllers/notification.controller.js";
 const router = Router();
 
 
 router.get("/viewNotification/:id",auth, updateNotification);
+router.get("/getNotification", getAllNotifications);
+
 
 export default router;
