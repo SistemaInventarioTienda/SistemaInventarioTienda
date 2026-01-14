2abc8e345b6119e37c4d18f3ff648b29ae48ff05
Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2026-01-14 15:45:28 -0600

    Update report.controller.js

--- ProyectoIngenieria/backend/controllers/report.controller.js
+++ ProyectoIngenieria/backend/controllers/report.controller.js
@@ -1529,33 +1529,29 @@ async function switchEXCEL(store, currentDate, type, MIN_FEC, MAX_FEC) {
       const data_client = Object.values(credit_Client);
 
       const client_Parsed = data_client.map((client) => {
-        let creditos = [];
-
-        try {
-          if (client.creditos_json != null) {
-            let fixedCreditosStr = `[${client.creditos_json}]`.replace(
-              /},\s*{/g,
-              "},{"
-            );
-            creditos = JSON.parse(fixedCreditosStr);
-          }
-        } catch (err) {
-          console.error(
-            "Error al parsear créditos para cliente:",
-            client.cliente_nombre,
-            err
-          );
-        }
-
         return {
           cedula: client.DSC_CEDULA,
-          nombre: client.cliente_nombre,
+
+          // cliente_nombre ahora es objeto JSON
+          nombre: client.cliente_nombre
+            ? `${client.cliente_nombre.nombre} ${client.cliente_nombre.apellido_uno} ${client.cliente_nombre.apellido_dos}`
+            : "",
+
           direccion: client.DSC_DIRECCION,
-          telefono: client.telefono,
-          creditos: creditos,
+
+          // telefonos ahora es array JSON
+          telefono: Array.isArray(client.telefonos)
+            ? client.telefonos.join(", ")
+            : "",
+
+          // creditos_json ahora es array JSON real
+          creditos: Array.isArray(client.creditos_json)
+            ? client.creditos_json
+            : [],
+
           cantidad_creditos: client.cantidad_creditos,
-          saldo_total_Pendiente: client.saldo_total_Pendiente,
-          abonos_total_Pagado: client.abonos_total_Pagado,
+          saldo_total_Pendiente: client.saldo_total_pendiente,
+          abonos_total_Pagado: client.abonos_total_pagado,
         };
       });
 
