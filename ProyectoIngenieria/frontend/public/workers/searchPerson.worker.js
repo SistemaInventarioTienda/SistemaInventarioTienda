306d1e4b1042da6f41799631ed806e9e39cec1f7
Anthony Daniel Briones Vargas <danielbrionesvargas1@gmail.com>
2026-03-12 21:06:59 -0600

    fix: Name format

--- ProyectoIngenieria/frontend/public/workers/searchPerson.worker.js
+++ ProyectoIngenieria/frontend/public/workers/searchPerson.worker.js
@@ -1,45 +1,46 @@
 // workers/searchPerson.worker.js
 
 onmessage = async function (event) {
-    try {
-        const response = await fetch(`https://api.hacienda.go.cr/fe/ae?identificacion=${event.data}`);
-        if (!response.ok) {
-            throw new Error("API response not OK");
-        }
-
-        const data = await response.json();
-        if (!data.nombre) {
-            throw new Error("No data found for the given ID");
-        }
-
-        const nombreCompleto = data.nombre.split(" ");
-        const nombre = capitalizar(nombreCompleto[0]);
-
-        let segundoNombre = "";
-        let apellidoUno = "";
-        let apellidoDos = "";
-
-        if (nombreCompleto.length === 4) {
-            segundoNombre = capitalizar(nombreCompleto[1]);
-            apellidoUno = capitalizar(nombreCompleto[2]);
-            apellidoDos = capitalizar(nombreCompleto[3]);
-        } else {
-            apellidoUno = capitalizar(nombreCompleto[1]);
-            apellidoDos = capitalizar(nombreCompleto[2]);
-        }
-
-        postMessage({
-            nombre,
-            segundoNombre,
-            apellidoUno,
-            apellidoDos,
-        });
-    } catch (error) {
-        console.error("Error en la solicitud:", error);
-        postMessage(null); // Indicar que hubo un error
+  try {
+    const response = await fetch(
+      `https://api.hacienda.go.cr/fe/ae?identificacion=${event.data}`,
+    );
+    if (!response.ok) {
+      throw new Error("API response not OK");
     }
+
+    const data = await response.json();
+    if (!data.nombre) {
+      throw new Error("No data found for the given ID");
+    }
+
+    const parts = data.nombre.trim().split(/\s+/).map(capitalizar);
+
+    const apellidoDos = parts.length > 0 ? parts.pop() : "";
+    const apellidoUno = parts.length > 0 ? parts.pop() : "";
+
+    let nombre = "";
+    let segundoNombre = "";
+
+    if (parts.length > 0) {
+      nombre = parts[0];
+      if (parts.length > 1) {
+        segundoNombre = parts.slice(1).join(" ");
+      }
+    }
+
+    postMessage({
+      nombre,
+      segundoNombre,
+      apellidoUno,
+      apellidoDos,
+    });
+  } catch (error) {
+    console.error("Error en la solicitud:", error);
+    postMessage(null); // Indicar que hubo un error
+  }
 };
 
 function capitalizar(cadena) {
-    return cadena.charAt(0).toUpperCase() + cadena.slice(1).toLowerCase();
+  return cadena.charAt(0).toUpperCase() + cadena.slice(1).toLowerCase();
 }
